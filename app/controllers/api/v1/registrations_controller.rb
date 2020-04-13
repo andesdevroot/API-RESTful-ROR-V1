6089410c764241c6c2e75c82395297a90eaa17ca
andesdevroot <andes.dev.root@gmail.com>
2020-04-13 08:53:51 -0400

    se testea api con postman en Post de User, todo ok

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -22,13 +22,13 @@ before_action :ensure_params_exist, only: :create
 
   private
   def user_params
-    params.require(:user).permit(:email, :password,:password_confirmation)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 
   def ensure_params_exist
     return if params[:user].present?
     render json: {
-      messages: "Missing params"
+      messages: "Missing params",
       is_success: false,
       data: {}
     }, status: :bad_request
