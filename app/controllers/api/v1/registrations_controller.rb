0a78e707edb82f1700248baa570d5c0200148d2a
andesdevroot <andes.dev.root@gmail.com>
2020-04-13 11:10:00 -0400

    se hace mas DRY el codigo en controlador API

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -4,19 +4,9 @@ before_action :ensure_params_exist, only: :create
   def create
     user = User.new user_params
     if user.save
-      render json: {
-        messages: "Registro exitoso",
-        is_success: true,
-        data: {
-          user: user
-        }
-      }, status: :ok
+     json_response "Registro exitoso", true, {user: user}, ok
     else
-      render json: {
-        messages: "Algo salio mal",
-        is_success: false,
-        data: {}
-      }, status: :unprocessable_entity
+     json_response "Algo salio mal", false, :unprocessable_entity
     end
   end
 
@@ -27,10 +17,6 @@ before_action :ensure_params_exist, only: :create
 
   def ensure_params_exist
     return if params[:user].present?
-    render json: {
-      messages: "Missing params",
-      is_success: false,
-      data: {}
-    }, status: :bad_request
+     json_response "Completar campos", false, {}, :bad_request
   end
 end
