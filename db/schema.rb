f26f8777dcfa3f7966c5c1fd2d2eb341dcd7870c
andesdevroot <andes.dev.root@gmail.com>
2020-04-18 20:24:08 -0400

    se crea model Books y se migra para conectar con API

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_17_154846) do
+ActiveRecord::Schema.define(version: 2020_04_19_001655) do
+
+  create_table "books", force: :cascade do |t|
+    t.string "image"
+    t.string "author"
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
